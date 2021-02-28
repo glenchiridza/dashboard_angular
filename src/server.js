f1f25eab5907f9e12f251a5a19fd3a76081511f6
glenchiridza <glenchiridz@gmail.com>
2021-02-28 12:58:21 +0200

    package update8

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/glenchiridza-dash.herokuapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/src/index.html'));
+res.sendFile(path.join('index.html',{root:'/dist/glenchiridza-dash.herokuapp/'}),
 });
 
 // Start the app by listening on the default Heroku port
